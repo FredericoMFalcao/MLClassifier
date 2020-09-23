d78fbeacdcbe7197fb5b52a4aad3a69081e5b5d6
Frederico <FredericoMFalcao@hotmail.com>
2020-09-23 15:33:24 +0100

    Update 01_Domains.sql

--- 01_tables/01_Domains.sql
+++ 01_tables/01_Domains.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Domains (
 	Name VARCHAR(255), -- e.g. "Invoices", "SalaryReceipts"
-	Length INT DEFAULT 1,
+	Length INT DEFAULT 1, -- i.e. no of learning data points for this domain
 	PRIMARY KEY (Name)
 );
 
