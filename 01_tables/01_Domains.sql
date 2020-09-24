6eb3a7cde90171aec109467092dff7114c6aa249
Frederico <FredericoMFalcao@hotmail.com>
2020-09-24 10:39:00 +0100

    Added column AutoCalculateCorrelations

--- 01_tables/01_Domains.sql
+++ 01_tables/01_Domains.sql
@@ -1,6 +1,11 @@
 CREATE TABLE Domains (
 	Name VARCHAR(255), -- e.g. "Invoices", "SalaryReceipts"
 	Length INT DEFAULT 1, -- i.e. no of learning data points for this domain
+
+	-- AutoCalculateCorrelations:
+	-- switch to either auto recalculate or not recalculate on every data insert
+	-- useful to bath insert data on first upload and not being slowed down by a recalculation on every row
+	AutoCalculateCorrelations INT DEFAULT 1, 
 	PRIMARY KEY (Name)
 );
 
