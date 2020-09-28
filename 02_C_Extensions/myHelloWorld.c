ccaf6945f3cb2a56c21e0607bcc323d711e21bac
Frederico Falcao <fmf@afm.co.ao>
2020-09-28 17:11:59 +0100

    added test c extensions

---
+++ 02_C_Extensions/myHelloWorld.c
@@ -0,0 +1,55 @@
+#ifdef STANDARD
+/* STANDARD is defined, don't use any mysql functions */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#else
+#include <my_global.h>
+#include <my_sys.h>
+#if defined(MYSQL_SERVER)
+#include <m_string.h>		/* To get strmov() */
+#else
+/* when compiled as standalone */
+#include <string.h>
+#define strmov(a,b) stpcpy(a,b)
+#define bzero(a,b) memset(a,0,b)
+#endif
+#endif
+#include <mysql.h>
+#include <ctype.h>
+
+#ifdef HAVE_DLOPEN
+
+#if !defined(HAVE_GETHOSTBYADDR_R) || !defined(HAVE_SOLARIS_STYLE_GETHOST)
+static pthread_mutex_t LOCK_hostname;
+#endif
+
+
+my_bool myhelloworld_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+void myhelloworld_deinit(UDF_INIT *initid);
+
+/***************************************************************************
+** UDF string function.
+** Arguments:
+** initid	Structure filled by xxx_init
+** args		The same structure as to xxx_init. This structure
+**		contains values for all parameters.
+**		Note that the functions MUST check and convert all
+**		to the type it wants!  Null values are represented by
+**		a NULL pointer
+** result	Possible buffer to save result. At least 255 byte long.
+** length	Pointer to length of the above buffer.	In this the function
+**		should save the result length
+** is_null	If the result is null, one should store 1 here.
+** error	If something goes fatally wrong one should store 1 here.
+**
+** This function should return a pointer to the result string.
+** Normally this is 'result' but may also be an alloced string.
+***************************************************************************/
+char *myhelloworld(UDF_INIT *initid __attribute__((unused)),
+               UDF_ARGS *args, char *result, unsigned long *length,
+               char *is_null, char *error __attribute__((unused)))
+{
+	strcpy(result, "Hello WORLD!");
+	return result;
+}
