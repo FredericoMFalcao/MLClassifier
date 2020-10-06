1031b8ec010cd6311415564a314af944ea7097e5
Frederico <FredericoMFalcao@hotmail.com>
2020-10-06 17:42:53 +0100

    Added option to limit the number of resulting rows

--- 03_procedures/03_Predict.sql
+++ 03_procedures/03_Predict.sql
@@ -5,7 +5,8 @@ DROP PROCEDURE IF EXISTS Predict;
 DELIMITER //
 CREATE PROCEDURE Predict (
 	IN _Domain VARCHAR(255),
-	IN _Inputs TEXT
+	IN _Inputs TEXT,
+	IN _LimitRows INT
 )
 BEGIN
 
@@ -17,6 +18,8 @@ BEGIN
 	INNER JOIN Inputs c		ON b.InputId = c.id AND JSON_CONTAINS(_Inputs, CONCAT('"',c.Name,'"'))
 	WHERE a.Domain = _Domain
 	GROUP BY a.id, a.Name
+	ORDER BY AVG(b.Length / c.Length) DESC
+	LIMIT _LimitRows
 
 	;
 END; 
