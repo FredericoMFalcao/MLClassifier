2e58694b7471d0efc75eb6a092a5c110364394fb
Frederico <FredericoMFalcao@hotmail.com>
2020-10-07 11:27:40 +0100

    Distinguished local variables from table/column names

--- 03_procedures/02_Learn.sql
+++ 03_procedures/02_Learn.sql
@@ -5,8 +5,8 @@ DROP PROCEDURE IF EXISTS Learn;
 DELIMITER //
 CREATE PROCEDURE Learn (
 	IN _Domain VARCHAR(255),
-	IN Category VARCHAR(255),
-	IN Inputs TEXT
+	IN _Category VARCHAR(255),
+	IN _Inputs TEXT
 )
 BEGIN
 	DECLARE _CategoryId INT;
@@ -24,21 +24,21 @@ BEGIN
 	
 	-- 2. Create new or update a "category"
 	-- ------------------------------------
-	IF NOT EXISTS (SELECT * FROM Categories WHERE Domain = _Domain AND Name = Category) THEN
+	IF NOT EXISTS (SELECT * FROM Categories WHERE Domain = _Domain AND Name = _Category) THEN
 		-- 2.1 Create new category
-		INSERT INTO Categories (Domain, Name) VALUES (_Domain, Category);
+		INSERT INTO Categories (Domain, Name) VALUES (_Domain, _Category);
 	ELSE
-		UPDATE Categories SET Length = Length + 1 WHERE Domain = _Domain AND Name = Category;
+		UPDATE Categories SET Length = Length + 1 WHERE Domain = _Domain AND Name = _Category;
 	END IF;
 	-- Fetch the ID number of the current category
-	SET _CategoryId = (SELECT id FROM Categories WHERE Domain = _Domain AND Name = Category);
+	SET _CategoryId = (SELECT id FROM Categories WHERE Domain = _Domain AND Name = _Category);
 	
 	-- 3. For each input, create new or update "InputPerCategory"
 	-- ------------------------------------
-	SET i = 0; WHILE i != JSON_LENGTH(Inputs) DO
+	SET i = 0; WHILE i != JSON_LENGTH(_Inputs) DO
 		-- 3.1 Extract current "input"
 		-- ------------------------------------
-		SET Input = JSON_VALUE(Inputs, CONCAT("$[",i,"]"));
+		SET Input = JSON_VALUE(_Inputs, CONCAT("$[",i,"]"));
 
 		-- 3.2 Create new or update each "input";
 		-- ------------------------------------
