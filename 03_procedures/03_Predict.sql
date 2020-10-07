94714c602b75c14b72fbe1218821e85a7e4b7665
Frederico <FredericoMFalcao@hotmail.com>
2020-10-07 10:46:19 +0100

    Improved query speed 10x using CTE.

--- 03_procedures/03_Predict.sql
+++ 03_procedures/03_Predict.sql
@@ -10,18 +10,25 @@ CREATE PROCEDURE Predict (
 )
 BEGIN
 
+	WITH WordList AS ( 
+	        SELECT a.id 
+	        FROM Inputs a 
+	        WHERE JSON_CONTAINS(_Inputs, CONCAT('"',a.Name,'"'))
+	)
 	SELECT 
-		a.Name AS Category, 
-		AVG(b.Length / c.Length) AS Probability
-	FROM Categories a
-	INNER JOIN InputPerCategory b 	ON a.id = b.CategoryId
-	INNER JOIN Inputs c		ON b.InputId = c.id AND JSON_CONTAINS(_Inputs, CONCAT('"',c.Name,'"'))
-	WHERE a.Domain = _Domain
-	GROUP BY a.id, a.Name
-	ORDER BY AVG(b.Length / c.Length) DESC
-	LIMIT _LimitRows
-
+                a.Name AS Category, 
+                AVG(b.Length / c.Length) AS Probability
+	        FROM Categories a
+	        INNER JOIN InputPerCategory b   ON a.id = b.CategoryId
+	        INNER JOIN Inputs c             ON b.InputId = c.id 
+	        INNER JOIN WordList d           ON c.id = d.id
+	        WHERE a.Domain = _Domain
+	        GROUP BY a.id, a.Name
+	        ORDER BY AVG(b.Length / c.Length) DESC
+		LIMIT _LimitRows
 	;
+
+
 END; 
 //
 DELIMITER ;
