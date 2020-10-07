ebbfcc19e8b1b3b60eb5a08292c1ded71d04d27d
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-10-07 14:11:22 +0100

    added function explain prediction

---
+++ 03_procedures/04_ExplainPrediction.sql
@@ -0,0 +1,33 @@
+-- --------------------
+-- - 4. Explaining Prediction 
+-- --------------------
+DROP PROCEDURE IF EXISTS ExplainPrediction;
+DELIMITER //
+CREATE PROCEDURE ExplainPrediction (
+	IN _Domain VARCHAR(255),
+	IN _CategoryName TEXT,
+	IN _Inputs INT
+)
+BEGIN
+
+
+        WITH ProbabilityPerWord AS ( 
+           SELECT 
+              c.Name as InputName, 
+              a.Length / c.Length AS Probability
+           FROM InputPerCategory a
+           INNER JOIN Categories b ON a.CategoryId = b.id
+           INNER JOIN Inputs c     ON a.InputId = c.id 
+           WHERE JSON_CONTAINS(_Inputs, CONCAT('"',c.Name,'"'))  AND b.Name = _CategoryName
+        )
+          SELECT  *
+          FROM ProbabilityPerWord
+        UNION ALL 
+	  SELECT 'AVERAGE', AVG(Probability)
+	  FROM ProbabilityPerWord
+	  GROUP BY 1;
+        
+
+END;
+//
+DELIMITER ;
