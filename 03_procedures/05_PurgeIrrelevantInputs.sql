7fb076c2de40f1c6d5f1afcdb0d5956bd6bd501c
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 16:27:58 +0100

    Create 05_PurgeIrrelevantInputs.sql

---
+++ 03_procedures/05_PurgeIrrelevantInputs.sql
@@ -0,0 +1,20 @@
+-- --------------------
+-- - 5. PurgeIrrelevantInputs
+--
+--   Description: when an input shows up in more than "_Threshold" categories, eliminate it
+--                example: threshold = 3,
+--                         categories = suppliers,
+--                         inputs     = words present in an invoice document
+--                         explanation: when the word "invoice" shows up in more than 3 suppliers' invoices, 
+--                                      eliminate it because it no longer helps distinguish suppliers
+--             
+-- --------------------
+
+CREATE PROCEDURE PurgeIrrelevantInputs(IN _Threshold INT)
+DELETE FROM Inputs 
+WHERE id IN (
+	SELECT b.InputId
+	FROM InputPerCategory b
+	GROUP BY b.InputId
+	HAVING COUNT(a.CategoryId) > _Threshold
+);
