caa9fce762a01b56beb62d47af18ca20d88485c9
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 23:05:23 +0100

    Update 03_Inputs.sql

--- 01_tables/03_Inputs.sql
+++ 01_tables/03_Inputs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Inputs (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	DomainId VARCHAR(255),
+	DomainId INT,
 	Name VARCHAR(255), -- e.g. "Drink", "Computer", "Hamburger"
 	Length INT DEFAULT 1,
 	FOREIGN KEY (DomainId) REFERENCES Domains(id) ON UPDATE CASCADE ON DELETE CASCADE,
