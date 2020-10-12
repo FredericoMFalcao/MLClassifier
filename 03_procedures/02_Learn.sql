e186494596918836a5a6c3ef872790b852700732
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 23:02:51 +0100

    Update 02_Learn.sql

--- 03_procedures/02_Learn.sql
+++ 03_procedures/02_Learn.sql
@@ -9,8 +9,7 @@ CREATE PROCEDURE Learn (
 	IN _Inputs TEXT
 )
 BEGIN
-	DECLARE _CategoryId INT;
-	DECLARE _InputId INT;
+	DECLARE _CategoryId, _InputId, _DomainId INT;
 	DECLARE i INT;
 	DECLARE Input VARCHAR(255);
 
@@ -21,17 +20,20 @@ BEGIN
 	ELSE
 		UPDATE Domains SET Length = Length + 1 WHERE Name = _Domain;
 	END IF;
+	-- Fetch the ID number of the current category
+	SET _DomainId = (SELECT id FROM Domains WHERE Name = _DomainId);
+
 	
 	-- 2. Create new or update a "category"
 	-- ------------------------------------
 	IF NOT EXISTS (SELECT * FROM Categories WHERE Domain = _Domain AND Name = _Category) THEN
 		-- 2.1 Create new category
-		INSERT INTO Categories (Domain, Name) VALUES (_Domain, _Category);
+		INSERT INTO Categories (DomainId, Name) VALUES (_DomainId, _Category);
 	ELSE
-		UPDATE Categories SET Length = Length + 1 WHERE Domain = _Domain AND Name = _Category;
+		UPDATE Categories SET Length = Length + 1 WHERE DomainId = _DomainId AND Name = _Category;
 	END IF;
 	-- Fetch the ID number of the current category
-	SET _CategoryId = (SELECT id FROM Categories WHERE Domain = _Domain AND Name = _Category);
+	SET _CategoryId = (SELECT id FROM Categories WHERE DomainId = _DomainId AND Name = _Category);
 	
 	-- 3. For each input, create new or update "InputPerCategory"
 	-- ------------------------------------
@@ -42,17 +44,17 @@ BEGIN
 
 		-- 3.2 Create new or update each "input";
 		-- ------------------------------------
-		IF NOT EXISTS (SELECT id FROM Inputs WHERE Domain = _Domain AND Name = Input) THEN
-			INSERT INTO Inputs (Domain, Name) VALUES (_Domain, Input);
-			SET _InputId = (SELECT id FROM Inputs WHERE Domain = _Domain AND Name = Input);
+		IF NOT EXISTS (SELECT id FROM Inputs WHERE DomainId = _DomainId AND Name = Input) THEN
+			INSERT INTO Inputs (DomainId, Name) VALUES (_DomainId, Input);
+			SET _InputId = (SELECT id FROM Inputs WHERE DomainId = _DomainId AND Name = Input);
 			-- Create a new InputPerCategory entry (if it's the 1st time this input appears with this category)
 			INSERT INTO InputPerCategory (CategoryId, InputId) 
 				VALUES (_CategoryId, _InputId);						  
 --				SELECT id, _InputId FROM Categories WHERE Domain = _Domain;
 		ELSE
-			UPDATE Inputs SET Length = Length + 1 WHERE Domain = _Domain AND Name = Input;
+			UPDATE Inputs SET Length = Length + 1 WHERE DomainId = _DomainId AND Name = Input;
 		END IF;
-		SET _InputId = (SELECT id FROM Inputs WHERE Domain = _Domain AND Name = Input);		
+		SET _InputId = (SELECT id FROM Inputs WHERE DomainId = _DomainId AND Name = Input);		
 		
 		-- 3.3 Create new or update each "InputPerCategory"
 		-- ------------------------------------
