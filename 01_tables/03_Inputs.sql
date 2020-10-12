86aae8e79885b4a4056f01cd8e0fd910b2e363a1
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 23:46:33 +0100

    Update 03_Inputs.sql

--- 01_tables/03_Inputs.sql
+++ 01_tables/03_Inputs.sql
@@ -3,6 +3,7 @@ CREATE TABLE Inputs (
 	DomainId INT,
 	Name VARCHAR(255) NOT NULL, -- e.g. "Drink", "Computer", "Hamburger"
 	Length INT DEFAULT 1,
+	LengthOfDifferentCategories INT DEFAULT 1,
 	FOREIGN KEY (DomainId) REFERENCES Domains(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	UNIQUE (DomainId, Name),
 	INDEX USING HASH (Name)
