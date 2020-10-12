0af9cfa3bae8c7f170eac116a9e4da50b41c1bf0
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 22:59:27 +0100

    Update 01_Domains.sql

--- 01_tables/01_Domains.sql
+++ 01_tables/01_Domains.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Domains (
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	Name VARCHAR(255), -- e.g. "Invoices", "SalaryReceipts"
 	Length INT DEFAULT 1, -- i.e. no of learning data points for this domain
 
@@ -6,6 +7,6 @@ CREATE TABLE Domains (
 	-- switch to either auto recalculate or not recalculate on every data insert
 	-- useful to bath insert data on first upload and not being slowed down by a recalculation on every row
 	AutoCalculateCorrelations INT DEFAULT 1, 
-	PRIMARY KEY (Name)
+	UNIQUE (Name)
 );
 
