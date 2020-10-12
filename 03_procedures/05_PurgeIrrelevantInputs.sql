611188092713e0cd35a1030897f60962505d1f24
Frederico <FredericoMFalcao@hotmail.com>
2020-10-12 19:51:28 +0100

    Fixed typo

--- 03_procedures/05_PurgeIrrelevantInputs.sql
+++ 03_procedures/05_PurgeIrrelevantInputs.sql
@@ -15,7 +15,7 @@ CREATE PROCEDURE PurgeIrrelevantInputs(IN _Threshold INT)
 DELETE FROM Inputs 
 WHERE id IN (
 	SELECT b.InputId
-	FROM InputPerCategory b
+	FROM InputsPerCategory b
 	GROUP BY b.InputId
 	HAVING COUNT(b.CategoryId) > _Threshold
 );
