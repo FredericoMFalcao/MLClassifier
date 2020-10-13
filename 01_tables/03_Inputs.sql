bc4d47488329bb50c7094f4566549be5f3393a44
Frederico <FredericoMFalcao@hotmail.com>
2020-10-13 12:40:08 +0100

    Added column

--- 01_tables/03_Inputs.sql
+++ 01_tables/03_Inputs.sql
@@ -4,7 +4,12 @@ CREATE TABLE Inputs (
 	Name VARCHAR(255) NOT NULL, -- e.g. "Drink", "Computer", "Hamburger"
 	Length INT DEFAULT 1,
 	LengthOfDifferentCategories INT DEFAULT 0,
+	WillBeDeleted BOOLEAN NOT NULL DEFAULT FALSE,
+	
+	-- Foreign keys:
 	FOREIGN KEY (DomainId) REFERENCES Domains(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	
+	-- Indexes / Constraints:
 	UNIQUE (DomainId, Name),
 	INDEX USING HASH (Name)
 );
